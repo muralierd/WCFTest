09b7757702f6405a3b89d94eca5a7214609b5adb
Murali Mani <Murali.Mani@thetrainline.com>
2018-04-30 12:25:14 +0530

    added files

---
+++ WcfServiceTestLibrary/EmployeeDetails.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Serialization;
+
+namespace WcfServiceTestLibrary
+{
+    [DataContract]
+    public class EmployeeDetails
+    {
+        [DataMember]
+        public string EmpId { get; set; }
+        [DataMember]
+        public string EmpName { get; set; }
+        [DataMember]
+        public string Address1 { get; set; }
+        [DataMember]
+        public string Address2 { get; set; }
+        [DataMember]
+        public string City { get; set; }
+        [DataMember]
+        public string State { get; set; }
+        [DataMember]
+        public long Pincode { get; set; }
+
+
+    }
+}
